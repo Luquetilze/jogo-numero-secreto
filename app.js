36a37077c9d1aad944a9d9608a8fa9e4f488948d
Luquetilze <lucascamposp@gmail.com>
2024-08-29 14:50:32 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let listaDeNumerosSorteados = [];
-let limiteMaximo = 10;
+let limiteMaximo = 100;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
@@ -10,7 +10,8 @@ function exibirTextoNaTela(tag, texto){
  
 function exibirMensagemInicial() {
     exibirTextoNaTela('h1' , 'Jogo do número secreto!');
-    exibirTextoNaTela('p' , 'Escolha um número entre 1 e 10.');
+    exibirTextoNaTela('p' , 'Escolha um número entre 1 e 100.');
+    responsiveVoice.speak(texto, 'Brazilian Portuguese Female', rate{1.2});
 }
 
 exibirMensagemInicial();
